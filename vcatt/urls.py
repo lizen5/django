ecd9397777b76637a0fe2acdad1baf6412308fe3
lizen5 <skyhopee2@gmail.com>
2022-12-20 19:44:26 +0900

    added homepage

--- vcatt/urls.py
+++ vcatt/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls')),
+    path('', include('single_pages.urls')),
 ]
