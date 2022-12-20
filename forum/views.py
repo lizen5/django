85fadee1ae44ef424993021b424ec5457d03afb2
lizen5 <skyhopee2@gmail.com>
2022-12-20 20:03:47 +0900

    going home

--- forum/views.py
+++ forum/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'forum/index.html',
-        {
-            'posts': posts,
-        }
-    )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
